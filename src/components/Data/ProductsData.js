5e215c4a0ed549331a67a9ffe27339716a64492c
Mr_R <dzforapp@gmail.com>
2021-10-29 20:10:48 +0100

    DataFetching

---
+++ src/components/Data/ProductsData.js
@@ -0,0 +1,37 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios"; 
+
+function PhotoFetching() {
+
+    const [photo, setPhoto] = useState({})
+    const [id, setId] = useState(1)
+
+    useEffect(() => {
+        axios
+            .get(`https://jsonplaceholder.typicode.com/photos/${id}`)
+            .then(res => {
+                console.log(res)
+                setPhoto(res.data)
+            })
+            .catch(err =>{
+                console.log(err)
+            })
+    }, [id])
+
+    return (
+        <div>
+            <input type="text" value={id} onChange={e => setId(e.target.value)}/>
+            <div>
+                
+                     {photo.title}
+                
+            </div> 
+            <img src = {photo.url} /> 
+                
+          
+            
+        </div>
+    )
+}
+
+export default PhotoFetching;
--- src/components/Pages/Products.jsx
+++ src/components/Pages/Products.jsx
@@ -1,9 +1,15 @@
 import React from "react"; 
+import PhotoFetching  from '../Data/ProductsData';
+import './Product.css'
+
 
 function Products (){
     return(
         <div className="products">
-            <h1>Products</h1>
+            <h5>Products</h5>
+            <div className="piclist">
+            <PhotoFetching />
+            </div>
         </div>
     )
 }
